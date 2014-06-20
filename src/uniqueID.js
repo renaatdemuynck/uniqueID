c50f1f4ab62f08c8315b7f80e2c248b71afee29e
Renaat De Muynck <renaat.demuynck@gmail.com>
2014-06-20 17:17:26 +0200

    Added the shim for IE's uniqueID

---
+++ src/uniqueID.js
@@ -0,0 +1,40 @@
+
+(function () {
+    
+    'use strict';
+    
+    var nextID = 1;
+    
+    if (document.uniqueID !== undefined) {
+        return;
+    }
+    
+    console.info('"document.uniqueID" not implemented; creating shim');
+    
+    // define a property on the document that will generate a unique id each time it is called
+    Object.defineProperty(Document.prototype, 'uniqueID', {
+        get: function () {
+            return nextID++;
+        },
+        enumerable: false,
+        configurable: false
+    });
+    
+    // define a property on the element that will return a unique id the first time it is called
+    Object.defineProperty(Element.prototype, 'uniqueID', {
+        get: function () {
+            // redefine the property so that the generator will not be called again
+            Object.defineProperty(Element.prototype, 'uniqueID', {
+                value: document.uniqueID,
+                writable: false,
+                enumerable: false,
+                configurable: false
+            });
+            
+            return this.uniqueID;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    
+}());
